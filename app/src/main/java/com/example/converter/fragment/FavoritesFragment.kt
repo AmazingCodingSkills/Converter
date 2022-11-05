9403519ca5a9ddc5a005eb141779d6db42f50032
Mikhail Kovalev <mikhail.kovalev@avisales.ru>
2022-11-05 21:37:44 +0700

    favorite_screen v1

---
+++ app/src/main/java/com/example/converter/fragment/FavoritesFragment.kt
@@ -0,0 +1,59 @@
+package com.example.converter.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.example.converter.R
+import com.example.converter.adapters.VpAdapter
+import com.example.converter.databinding.FragmentFavouritesBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class FavoritesFragment() : Fragment() {
+    private val flist = listOf(
+        TabLayoutFragmentFavorites.newInstance(),
+        TabLayoutFragmentFavoritesAll.newInstance()
+    )
+    private val tList = listOf(
+        "Избранное",
+        "Все"
+    )
+    private lateinit var binding: FragmentFavouritesBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFavouritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+
+        binding.arrowBackMain.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, LatestValueFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, flist)
+        viewPager.adapter = adapter
+        TabLayoutMediator(tablayout, viewPager) {
+tab, pos -> tab.text = tList[pos]
+        }.attach()
+    }
+}
+
+
+
+
