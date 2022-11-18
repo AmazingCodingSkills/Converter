5ba8b64509b575cd68223a963f23dc0ea9ce8c4f
Mikhail Kovalev <mikhail.kovalev@avisales.ru>
2022-11-18 16:20:32 +0700

    BottomSheet with RecyclerView

---
+++ app/src/main/java/com/example/converter/countryname/Country.kt
@@ -0,0 +1,6 @@
+package com.example.converter.countryname
+
+data class Country(
+    val flag: Int,
+    val nameCountry: String
+)
---
+++ app/src/main/java/com/example/converter/fragment/BottomSheet.kt
@@ -0,0 +1,56 @@
+package com.example.converter.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.converter.R
+import com.example.converter.countryname.CountryAdapter
+import com.example.converter.databinding.FragmentBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class BottomSheet : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCountry()
+    }
+    private fun initCountry(){
+        binding.apply {
+            recyclerFavorite.layoutManager = LinearLayoutManager(activity)
+            recyclerFavorite.adapter = CountryAdapter(getCountryList())
+            getCountryList()
+        }
+    }
+    private fun getCountryList(): List<String> {
+        return this.resources.getStringArray(R.array.countries_list).toList()
+    }
+
+
+    /*private fun initListView() = with(binding) {
+
+        val countries: Array<String> = resources.getStringArray(R.array.countries_list)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.activity_list_item, countries)
+
+        listItemCountries.adapter = arrayAdapter
+        listItemCountries.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, position, id ->
+                val selectedItem = adapterView.getItemAtPosition(position) as String
+                val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+            }
+    }*/
+
+
+}
