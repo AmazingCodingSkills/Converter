ae3c938bfc918b5ebcf9a2d6f93f4b67f4745326
Sergey Kurchenko <ksvdvlpr@gmail.com>
2023-02-16 13:16:29 +0700

    Убрал черную магию в NetworkAvailabilityDialogFragment + решил изменить момент с тем, что логика в onStart находится. Сделал функцию и запускаю ее в onViewCreated.

--- app/src/main/java/com/currency/converter/base/NetworkAvailabilityDialogFragment.kt
+++ app/src/main/java/com/currency/converter/base/NetworkAvailabilityDialogFragment.kt
@@ -11,22 +11,29 @@ import com.example.converter.R
 import com.example.converter.databinding.DialogCustomBinding
 
 
-class NetworkAvailabilityDialogFragment: DialogFragment() {
+class NetworkAvailabilityDialogFragment : DialogFragment() {
 
     private lateinit var binding: DialogCustomBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.dialog_corner)
         binding = DialogCustomBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
-        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
-        dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialogWork()
+    }
+    private fun dialogWork() {
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
         binding.repeatCheckInternetBtn.setOnClickListener {
             dismiss()
         }
@@ -39,6 +46,7 @@ class NetworkAvailabilityDialogFragment: DialogFragment() {
         }
     }
 }
+
 
 
 
